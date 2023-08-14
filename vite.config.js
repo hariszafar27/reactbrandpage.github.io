5ffa2fae1515cbfe342ea151931c303ba81c1c65
hariszafar27 <111231586+hariszafar27@users.noreply.github.com>
2023-08-15 01:59:23 +0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default {
+  build: {
+    outDir: 'build', // Change this to 'build'
+  },
+};
